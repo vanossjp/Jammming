aa7f661ce0ce4a314bce09cd9c11837842c778fd
Jessie van Oss <vanossjp@Jessies-MacBook-Pro.local>
2018-10-07 10:18:21 -0400

    Spotify acess token & expiration match results added

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -6,11 +6,15 @@ const Spotify = {
   async getAccessToken() {
     if (userAccessToken) {
     return userAccessToken;
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+/*
   }else{
     const urltoFetch = 'https://accounts.spotify.com/authorize?'
     const queryResponseType = 'response_type=token';
     const queryRedirect = 'redirect_uri=http://localhost:3000';
     const scopes = 'user-read-private playlist-modify-private'
+*/
   }
   }
 };
